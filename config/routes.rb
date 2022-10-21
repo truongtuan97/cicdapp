ef453251bfb6b4788325a02179f0adbbc4f0c7a7
tuan truong <tuan.truong@texo.design>
2022-10-21 11:14:41 +0700

    update routes and add system test

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'static_pages/landing_page'
-  get 'static_pages/pricing'
+  root 'static_pages#landing_page'
+  get 'pricing', to: 'static_pages#pricing'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
